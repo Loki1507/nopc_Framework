6d42c54538407fe25533425cbbff0c2f03c552f3
chintaLokesh <chintalokesh1507@gmail.com>
2026-02-20 19:54:24 +0530

    added launch page

---
+++ tests/test1.spec.js
@@ -0,0 +1,10 @@
+import {test, expect} from '@playwright/test';
+
+test('verify login page', async ({page}) => {
+
+    await page.goto('https://demo.nopcommerce.com/');
+    await page.getByRole('link', {name: 'Log in'}).waitFor();
+    await page.getByRole('link', {name: 'Log in'}).click();
+    await page.pause()
+}
+);
